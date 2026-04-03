09b7794452da2385ae3d0969aa3e732fc8acdf23
Nao <92193274+NaoRYK@users.noreply.github.com>
2026-04-03 03:34:49 -0300

    Agregar archivos de proyecto.

---
+++ pryGoodHardChalimond/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace pryGoodHardChalimond
+{
+    partial class frmMigracion
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMigration = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnMigration
+            // 
+            this.btnMigration.Location = new System.Drawing.Point(12, 50);
+            this.btnMigration.Name = "btnMigration";
+            this.btnMigration.Size = new System.Drawing.Size(510, 43);
+            this.btnMigration.TabIndex = 0;
+            this.btnMigration.Text = "Iniciar migración";
+            this.btnMigration.UseVisualStyleBackColor = true;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(207, 116);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(120, 13);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Información del proceso";
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Location = new System.Drawing.Point(24, 141);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.ReadOnly = true;
+            this.txtInfo.Size = new System.Drawing.Size(488, 170);
+            this.txtInfo.TabIndex = 2;
+            // 
+            // frmMigracion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 326);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnMigration);
+            this.Name = "frmMigracion";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmMigracion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMigration;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox txtInfo;
+    }
+}
+
